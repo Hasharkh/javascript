83332d9c9f4e56a558b608fdc4e107106adf78ca
Hasharkh <khawajahasharahmed@gmail.com>
2024-11-28 11:04:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=144553 //const ka mtlb ab value change ni ho skti
+let accountEmail="khawajahasharahmed@gmail.com"
+var accountPassword="12345"
+accountCity="Muzaffarabad"
+
+//accountId=2 //this will give error because const can not be change
+accountEmail="Khawajahashar.com"
+accountPassword="00000"
+accountCity="Kashmir"
+console.table([accountId, accountEmail, accountPassword, accountCity])
