a01b306adfd774bd770b2ddc0152689539e0be79
Hasharkh <khawajahasharahmed@gmail.com>
2024-11-28 11:04:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+/*number=> its range is 2 to power 23
+bigint
+string => ""
+boolean => true/false
+null => stand alone value
+undefined => value not assigned
+symbol => unique
+
+-------------------------------------------------------------------------------------
+Object
