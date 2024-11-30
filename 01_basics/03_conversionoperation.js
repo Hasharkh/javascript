1fc13381dc8fa41afe3a0f783e052de17e6df9a1
Hasharkh <khawajahasharahmed@gmail.com>
2024-11-30 10:28:13 +0000

    Conversion opertion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let num = "33"
+console.log(typeof(num))
+let valueinNumber = Number(num)
+console.log(typeof valueinNumber);
+
+// "33" => 33
+// "33abs" => nan
+// true => 1
+// false => 0
+let isLoggedIn = 1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// isLoggedIn = 1  Boolean(isLoggedIn)  ==> true
+// isLoggedIn = ""  Boolean(isLoggedIn)  ==> false
+// isLoggedIn = "hashar"  Boolean(isLoggedIn)  ==> true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
